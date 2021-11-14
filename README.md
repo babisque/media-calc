d3be102ebbb8b63e9c58865261ca3c2742041ec7
Babisque <rodrigobabisque1@gmail.com>
2021-11-13 23:07:52 -0300

    sistema final com validação de entrada

---
+++ README.md
@@ -0,0 +1,22 @@
+# Atividade Introdução a Programação de Computadores
+Atividade desenvolvida para a matéria de Introdução a Programação de Computdaores no curso de Análise e Desenvolvimento de Sistemas pela Universidade Vila Velha.
+
+## Faça um algoritmo em Python que leia de cem (100) alunos de um curso EAD da UVV:
+* Nota [0, 1] na AOP1: Atividade Online Pontuada 1; 
+* Nota [0, 2] na AOP2: Atividade Online Pontuada 2; 
+* Nota [0, 1] na AOP3: Atividade Online Pontuada 3; 
+* Nota [0, 6] da PROVA REGULAR;
+* Nota [0, 10] da PROVA DE RECUPERAÇÃO: Se o alunos não obteve Média do Módulo (MM) = AOP1 + APO2 + AOP3 + PROVA REGULAR >= 7.0 
+
+### E, calcule e mostre na tela: as informações a seguir: 
+
+* Média do Módulo (MM) = AOP1 + APO2 + AOP3 + PROVA REGULAR
+* Status do Aluno: Aprovado ou Reprovado;
+* A quantidade de alunos (em porcentagem) que foram: Aprovado ou Reprovado; 
+
+### Lembrando que, é considerado com Status Aprovado, o aluno que: 
+
+* Obteve Média do Módulo >= 7.0 ou 
+* Obteve Média Geral (com a Prova de Recuperação) >= 5.0 
+
+OBS: Para testar seu programa reduzir a quantidade de alunos da turma para 5 alunos, apenas. 
---
+++ main.py
@@ -0,0 +1,95 @@
+"""
+script feito em Python 3.9
+"""
+import os
+
+aprovados = []
+reprovados = []
+
+while True:
+    aluno = str(input("Nome do aluno: "))
+
+    while True:
+        try:
+            aop1 = float(input("Atividade Online Pontuada 1: "))
+            if not 0 <= aop1 <= 1:
+                os.system('clear') or None
+                raise ValueError("Nota inválida")
+            break
+        except ValueError as error:
+            print(error)
+
+    while True:
+        try:
+            aop2 = float(input("Atividade Online Pontuada 2: "))
+            if not 0 <= aop2 <= 2:
+                os.system('clear') or None
+                raise ValueError("Nota inválida")
+            break
+        except ValueError as error:
+            print(error)
+
+    while True:
+        try:
+            aop3 = float(input("Atividade Online Pontuada 3: "))
+            if not 0 <= aop3 <= 1:
+                os.system('clear') or None
+                raise ValueError("Nota inválida")
+            break
+        except ValueError as error:
+            print(error)
+
+    while True:
+        try:
+            prova = float(input("Prova: "))
+            if not 0 <= prova <= 6:
+                os.system('clear') or None
+                raise ValueError("Nota inválida")
+            break
+        except ValueError as error:
+            print(error)
+
+    media_modulo = aop1 + aop2 + aop3 + prova
+
+    if (media_modulo >= 7):
+        print("O aluno {} foi APROVADO com {} pontos".format(aluno, media_modulo))
+        aprovados.append(aluno)
+    else:
+        print("O aluno {} foi REPROVADO com {} pontos, e precisa fazer a recuperação.".format(aluno, media_modulo))
+
+        while True:
+            try:
+                recuperacao = float(input("Nota Recuperação: "))
+                if not 0 <= recuperacao <= 10:
+                    os.system('clear') or None
+                    raise ValueError("Nota inválida")
+                break
+            except ValueError as error:
+                print(error)
+
+        media_geral = (media_modulo + recuperacao) / 2
+
+        if (media_geral >= 5):
+            print("O aluno {} foi APROVADO com {} de média geral".format(aluno, media_geral))
+            aprovados.append(aluno)
+        else:
+            print("O aluno {} REPROVOU na recuperação com {} de média geral".format(aluno, media_geral))
+            reprovados.append(aluno)
+
+    print("-" * 48)
+
+    continuar = str(input("Gostaria de calcular mais uma nota? [S/n] "))
+
+    if(continuar == "n"):
+        print("-" * 48)
+        break
+
+    print("-" * 48)
+
+os.system('clear') or None
+
+total_alunos = len(aprovados) + len(reprovados)
+aprovados_pcento = (100 / total_alunos) * len(aprovados)
+reprovados_pcento = (100 / total_alunos) * len(reprovados)
+
+print("{:.2f}% alunos foram APROVADOS e {:.2f}% ficaram REPROVADOS".format(aprovados_pcento, reprovados_pcento))
